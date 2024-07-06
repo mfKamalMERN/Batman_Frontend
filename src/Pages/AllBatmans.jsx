25c427a4db19476d9faccf9a62c2b81448b29a01
mufaisa <mfkamalimdia@gmail.com>
2024-07-06 22:48:50 +0530

    changes

--- src/Pages/AllBatmans.jsx
+++ src/Pages/AllBatmans.jsx
@@ -54,7 +54,7 @@ export const AllBatmans = () => {
 
                             <div className="singlebatman" style={{ display: "flex", background: "black", color: "wheat", justifyContent: "space-between", alignItems: 'center', border: "1px solid wheat", borderRadius: "15px" }}>
 
-                                <div className="btmaninfo" style={{ display: "flex", color: "wheat", alignItems: "center", height: "55px",marginLeft:"3%" }}>
+                                <div className="btmaninfo" style={{ display: "flex", color: "wheat", alignItems: "center", height: "55px", marginLeft: "3%" }}>
                                     <img src={batman.DP} alt="" style={{ width: "38%", borderRadius: '180px', height: "50px" }} />
                                     <p>{batman.Name}</p>
                                 </div>
