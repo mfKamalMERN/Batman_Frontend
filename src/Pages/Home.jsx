13f5965a1e221eb9fa6b866f228c4ae84e3a5ed4
mufaisa <mfkamalimdia@gmail.com>
2024-07-14 05:15:37 +0530

    changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -180,7 +180,7 @@ export const Home = () => {
             <div className="home" style={{ backgroundColor: "black", minHeight: "190vh" }}>
 
                 {
-                    !datas?.data?.Token ?
+                    !datas?.data?.Token?
                         tokenChecker()
                         :
                         <PostsDisplayer values={{ AddNewComment, setNewcomment, newcomment, FollowUnfollow, RemoveComment, editStatussetter, UpdateComment, setUpdatedcomment, updatedcomment, commentid, editstatus, postid, commentstatus, likes, likesstatus, viewComments, ViewLikesSetter, LikeUnlike }} posts={datas.data.AllPosts} batmans={datas?.data?.AllBatman} postsFetcher={postsFetcher} />
